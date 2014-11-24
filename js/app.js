a6e6c7370658a308883fda4bddedb1bbde3ac57b
krj5063 <krj5063@gmail.com>
2014-11-24 18:44:05 -0500

    finishing touches
    finishing touches

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
     
+    $('.maintext').animate({"top": "+150px"}, 2000,                            function()  {               
+                           });
     
     $('.ryu').mouseenter(function() {
         $('.ryu-still').hide();
         $('.ryu-ready').show();
-    })
+        })
     .mouseleave(function() {
         $('.ryu-ready').hide();
         $('.ryu-throwing').hide();
@@ -14,23 +16,28 @@ $(document).ready(function() {
     $('.ryu').mousedown(function() {
         playHadouken ();
        $('.ryu-ready').hide();
+        $('.ryu-cool').hide();
+        $('.maintext').hide();
         $('.ryu-throwing').show();
         $('.hadouken').show();
         $('.hadouken').finish().show()
         .animate({"left": "+400px"}, "slow",                            function() {$('.hadouken').hide();
             $('.hadouken').removeAttr('style');                       });
-    })
+        })
     .mouseup(function() {
         $('.ryu-throwing').hide();
         $('ryu-ready').show();
+        $('.maintext').show();
     });
 
-$(this).keydown(function (event) {
+$(HTMLBodyElement).keydown(function (event) {
    var key = event.which;
    if (key == 88) {  
       $(".ryu-still").hide();
+       $(".ryu-ready").hide();
+       $(".ryu-throwing").hide();
       $(".ryu-cool").show();
-   }})   
+   }})  
 
 });
 
